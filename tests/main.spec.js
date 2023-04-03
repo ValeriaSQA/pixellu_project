9d9590dd784a7b4873b6433d82d5ef1fa7a03973
Valeriia Sysoeva <121239421+ValeriaSQA@users.noreply.github.com>
2023-04-03 19:35:36 +0700

    Update main.spec.js

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,13 +2,13 @@ const { test, expect } = require('@playwright/test');
 const { Login } = require('../page-objects/Login.js');
 
 test.describe('Test task', async () => {
-    test.beforeEach(async ({ page }) => {
+    test.beforeEach( async ({ page }) => {
         const login = new Login(page);
         await login.goToMainPage();
         await expect(page.locator('[class="page-title"]')).toHaveText('Make beautiful slideshows');
     });
 
-    test( 'Positive login check', async({ page }) => {
+    test('Positive login check', async({ page }) => {
         const login = new Login(page);
         await login.login();
         await login.enterEmail();
@@ -16,7 +16,7 @@ test.describe('Test task', async () => {
         await login.slideShowsScreenDisplaying();
     });
 
-    test( 'Negative login check', async({ page }) => {
+    test('Negative login check', async({ page }) => {
         const login = new Login(page);
         await login.login();
         await login.enterEmptyValue();
