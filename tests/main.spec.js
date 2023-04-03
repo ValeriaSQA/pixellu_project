a2ab1ecb8e1e071904007b2edb36e5618d2c42fd
Valeriia Sysoeva <121239421+ValeriaSQA@users.noreply.github.com>
2023-04-03 19:38:40 +0700

    Update main.spec.js

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 const { Login } = require('../page-objects/Login.js');
 
 test.describe('Test task', async () => {
-    test.beforeEach( async ({ page }) => {
+    test.beforeEach( async({ page }) => {
         const login = new Login(page);
         await login.goToMainPage();
         await expect(page.locator('[class="page-title"]')).toHaveText('Make beautiful slideshows');
